a7eb72a10e99a337d1876245972f8125fee9fb93
DLew <96327749+DLew2024@users.noreply.github.com>
2024-01-16 17:29:36 -0600

    FInished Javascript

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector("#push").onclick = function () {
+  // Adding input validation
   if (document.querySelector("#newtask input").value.length == 0) {
-    alert("Please Enter a Task");
+    alert("Please Enter A Task");
   } else {
     document.querySelector("#tasks").innerHTML += `
             <div class="task">
@@ -13,20 +14,21 @@ document.querySelector("#push").onclick = function () {
             </div>
         `;
 
+    // Deleting task
     var current_tasks = document.querySelectorAll(".delete");
     for (var i = 0; i < current_tasks.length; i++) {
       current_tasks[i].onclick = function () {
         this.parentNode.remove();
       };
     }
-
+    // Crossin out completed task
     var tasks = document.querySelectorAll(".task");
     for (var i = 0; i < tasks.length; i++) {
       tasks[i].onclick = function () {
         this.classList.toggle("completed");
       };
     }
-
+    // Clearing input after each entry
     document.querySelector("#newtask input").value = "";
   }
 };
